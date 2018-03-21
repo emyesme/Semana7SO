db18eaf9a74f4605c6ba3450c44b5ec789745452
emyesme <emily.carvajal@correounivalle.edu.co>
2018-03-21 17:32:19 -0500

    Código Semana 7 sistemas operativos

---
+++ semana7so.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ps ax me muestra todos los procesos que corren en mi sistema
+
+numProcesos=$(ps ax | tail -n +2 | wc -l)
+
+echo ${numProcesos}
+
+#memoria libre
+
+cantMemoria=$(free | grep Memoria: | awk '{print ($4/$2)*100}')
+echo ${cantMemoria}
+
+#disco libre
+discoLibre=$(df | grep root | awk '{print (($4-$3)/$4)*100.0}')
+
+echo ${discoLibre}
+
+#crontab -e tareas que se van a correr y su periodicidad
+
+#instruccion para subir la informacion
+curl --silent --request POST --header "X-THINGSPEAKAPIKEY: 2IX0C4A24J3V7NRO" --data "field1=${numProcesos}&field2=${cantMemoria}&field3=${discoLibre}" http://api.thingspeak.com/update
+
